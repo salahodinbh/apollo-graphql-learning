b2e54d321ff3995358f2c5fc4f8f977fe744ebe2
Stryfura <shilunett@gmail.com>
2025-07-19 23:59:00 +0200

    added query to get tracks

--- client/src/pages/tracks.tsx
+++ client/src/pages/tracks.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 import { Layout } from "../components";
+import { gql } from "../__generated__";
+
+/** TRACKS query to retrieve all tracks */
+const TRACKS = gql(`
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+    }
+  }
+`);
 
 /**
  * Tracks Page is the Catstronauts home page.
